83e6e1a8709da0ed0c316dde4d64bae956b59521
rodrigo-turnes-parser <rodrigo.turnes@parserdigital.com>
2022-11-18 13:59:16 +0100

    Adding cookie after response

--- src/datawall.js
+++ src/datawall.js
@@ -4,10 +4,30 @@ function dismissModal() {
     document.getElementById("blocker-container").style.display = "none";
     // document.getElementById("modal-background").style.display = "none";
     document.body.style.overflow = 'scroll';
+    var today = new Date();
+    var expire = new Date();
+    expire.setDate(today.getDate() + 14);
+    document.cookie = "responded=true; expires=" + expire.toGMTString();
 }
 
 function showModal() {
-    document.getElementById("blocker-container").style.display = "block";
-    // document.getElementById("modal-background").style.display = "flex";
-    document.body.style.overflow = 'hidden';
+    if (!userHasResponded()) {
+        document.getElementById("blocker-container").style.display = "block";
+        // document.getElementById("modal-background").style.display = "flex";
+        document.body.style.overflow = 'hidden';
+    }
+}
+
+function userHasResponded() {
+    var cookies = document.cookie;
+
+    cookies.split(';').forEach(cookie => {
+        var parts = cookie.split('=');
+
+        if (parts[0] === 'responded') {
+            return parts[1] === 'true';
+        }
+    });
+
+    return false;
 }
